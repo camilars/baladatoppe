844c62814d00e7d57826c737c1ac165d8953e173
camilars <camila.rs122@gmail.com>
2018-03-21 13:12:51 -0300

    alterei a cor do icone do github que estava azul

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
 <div class="jumbotron text-center">
 <footer><i class="fas fa-envelope"></i> contato@accesspremiun.com  <i class="fas fa-phone"></i> +55 0800 2330 300
+<style type="text/css"> 
+.fa-github {
+	color: black;
+}
+</style>
 <a href="https://github.com/camilars/baladatoppe"><i class="fab fa-github"></i></a>
 </footer>
 </div>
