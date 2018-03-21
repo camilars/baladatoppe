e6e85008786faffb373b3fe2b83dfc8d75c960f0
pedrolucass99 <pedrorochi73@gmail.com>
2018-03-21 07:52:36 -0300

    editado por pedro

--- colaboradores.php
+++ colaboradores.php
@@ -3,7 +3,7 @@
   	
   	<h1>Colaboradores</h1>
 		<h2>Camila Rodrigues</h2>
-		<h2>Predo Lucas</h2>
+		<h2>Pedro Lucas</h2>
 		<h2>Rafael Herculano</h2>
 		<h2>Maria Luiza</h2>
 		<h2>Carlos Miguel</h2>
