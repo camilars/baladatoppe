d854ad90eb7419d0613ae560dc76bec98ef5ebb8
pedrolucass99 <pedrorochi73@gmail.com>
2018-03-21 07:31:06 -0300

    editado por pedro

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+</head>
+<body>
+	<form action="formulario.php" method="POST">
+		<input type="text" name="login" placeholder="Login">
+		<input type="password" name="senha" placeholder="senha">
+		<input type="submit" value="Enviar">
+	</form>
+</body>
+</html>
