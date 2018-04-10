df15027769ddebdb6d21e317aa5cc17b8d60f41f
RafaelHeculano <rafaelh.silva31@gmail.com>
2018-04-10 10:06:42 -0300

    Editado por Rafael e Pedro a opcao colaboradores access premium

--- acess preimiun/colaboradores.php
+++ acess preimiun/colaboradores.php
@@ -8,12 +8,57 @@
 <body>
 <div id="colaborares" class="text-center">
   <h2>Colaboradores</h3><br>
-    <h3>Camila Rodrigues</h3>
-    <h3>Pedro Lucas</h3>
-	<h3>Rafael Herculano</h3>
-	<h3>Maria Luiza</h3>
-	<h3>Carlos Miguel</h3>
+ 
 </div>
+
+
+
+	
+
+ 	
+ </center>
+
+	<div class="row text-center">
+    <div class="col-sm-3">
+      <p class="text-center"><strong>Camila Rodrigues</strong></p><br>
+        <img src="imagens/mila.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+      <div>
+        <p><i class="fas fa-envelope"></i> camila.rs122@gmail.com</p>
+        <p><i class="fab fa-whatsapp"></i> 081 98461 6717</p>
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <p class="text-center"><strong>Pedro Lucas</strong></p><br>
+        <img src="imagens/avatar.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+      <div>
+        <p><i class="fas fa-envelope"></i> pedrolucastitan@hotmail.com</p>
+        <p><i class="fab fa-whatsapp"></i> 081 98510 3107</p>
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <p class="text-center"><strong>Rafael Herculano</strong></p><br>
+        <img src="imagens/fael.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+      <div>
+        <p><i class="fas fa-envelope"></i> rafaelh.silva31@gmail.com</p>
+        <p><i class="fab fa-whatsapp"></i> 081 98487 0887</p>
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <p class="text-center"><strong>Maria Luiza</strong></p><br>
+        <img src="imagens/avatar.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+      <div>
+        <p><i class="fas fa-envelope"></i> malu21linceluiza@gmail.com</p>
+        <p><i class="fab fa-whatsapp"></i> 081 99118 0172</p>
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <p class="text-center"><strong>Carlos Miguel</strong></p><br>
+        <img src="imagens/miguel.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+      <div>
+        <p><i class="fas fa-envelope"></i> carlos.miguel.r@outlook.com</p>
+        <p><i class="fab fa-whatsapp"></i> 081 98632 8994</p>
+      </div>
+
 <?php include 'footer.php' ; ?>
 </body>
 </html>
