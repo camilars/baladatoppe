81766269c17284a715ea96e8d54705a6dd1ece92
maria luiza1 <malu21linceluiza@gmail.com>
2018-04-11 13:51:00 -0300

    formulario de cadastro feito

--- header.php
+++ header.php
@@ -51,7 +51,7 @@
         </li>
         <!--  tag li para ir no mapa -->
         <li><a href="mapa.php"><i class="fas fa-map-marker-alt"></i></span></a></li>
-        <li><a href="#formulario">CADASTRE-SE</a></li>
+        <li><a href="form_cadastro.php">CADASTRE-SE</a></li>
         <li><a href="Login editado e css/login.php">LOGIN <i class="fas fa-sign-in-alt"></i></a></li>
       </ul>
     </div>
