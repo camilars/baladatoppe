774280de8cce1f4a29e03aed8aa0837dcaf40bf7
camilars <camilars.122@gmail.com>
2018-04-12 19:07:58 -0300

    inclir botão de cadastro no index

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-
--<?php include 'header.php'; ?>
+<?php include 'header.php'; ?>
 
  
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -16,15 +15,15 @@
       <div class="item active">
         <img src="imagens/imag1.jpg" width="1200" height="700">
         <div class="carousel-caption">
-          <h3>Nome do local da balada</h3>
-          <p>descrição...........................</p>
+          <h2>Quer conheçer as melhores baladas e participar de PROMOÇÕES?</h2>
+          <a href="form_cadastro.php" class="btn btn-danger" role="button">CADASTRE-SE</a>
         </div>      
       </div>
 
       <div class="item">
        <img src="imagens/imag3.jpg" width="1200" height="700">
         <div class="carousel-caption">
-          <h3>Nome do local da balada</h3>
+          <h3></h3>
           <p>descrição...........................</p>
         </div>      
       </div>
@@ -37,14 +36,11 @@
         </div>      
       </div>
     </div>
-?>
- 
     <!-- controle do carousel -->
     <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
       <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a href="acess preimiun/">ss</a>
     <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
       <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
@@ -57,11 +53,6 @@
   <br>
   <br>
 </div>
-<button>
-	
-<a href="login.php">Sair</a> 
-</button>
- 
-    </div>
-  </div>
+</div>
+</div>
 </div> 
