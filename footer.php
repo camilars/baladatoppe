7c811ea5b74c70cf1fc07aa745fd21d18b6cb63e
camilars <camila.rs122@gmail.com>
2018-04-17 22:20:27 -0300

    incluir o arquivo do rodapé

--- footer.php
+++ footer.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Balada Top </title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="index.css">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="index.css">
   <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <script src="js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="js/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -16,16 +13,27 @@
   <script src="fontawesome/svg-with-js/js/fontawesome-all.min.js"></script>
 </head>
 <body>
-<nav class="navbar navbar-expand-sm navbar-light bg-faded">
-   <a class="navbar-brand" href="index.php">Balada Top</a>
-  </div>
-    <div class="collapse navbar-collapse justify-content-end" id="nav-content">
-      <ul class="nav navbar-nav navbar-right">
-        <li class="nav-item"><a href="index.php"><i class="fas fa-home"></i> HOME </a></li>
-        <li class="nav-item"><a href="mapa.php"><i class="fas fa-map-marker-alt"></i> MAPA </a></li>
-        <li class="nav-item"><a href="form_cadastro.php"> CADASTRE-SE </a></li>
-        <li class="nav-item"><a href="login.php"> LOGIN <i class="fas fa-sign-in-alt"></i></a></li>
-      </ul>
+	<footer class="page-footer font-small stylish-color-dark pt-4 mt-4">
+		<div class="container-fluid text-center text-md-left">
+        <div class="row">
+        	<div class="col-md-6">
+                <h5 class="text-uppercase">Balada Top</h5>
+                <p>Uma aplicação que vai ajudar você a localizar as melhores baladas.</p>
+          </div>
+          <div class="col-md-6">
+                <h5 class="text-uppercase">Empresa desenvolvedora</h5>
+                <ul class="list-unstyled">
+                  <li>
+                    <a href="../access-premiun/index.php">www.accesspremiun.com</a>
+                  </li>
+          </div>
+        </div>
+    <div class="footer-copyright py-3 text-center">
+        © 2018 Copyright:
+        <a href="http://www.ifpe.edu.br/campus/igarassu">IFPE-Igarassu<a>
     </div>
-  </div>
-</nav>
+
+		
+	</footer>
+</body>
+</html>
