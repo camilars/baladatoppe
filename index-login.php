da7b73d9d0be93bb21c343866c5bdcdb95d16c94
camilars <camila.rs122@gmail.com>
2018-04-19 09:29:29 -0300

    alterado o cabeçalho e menu das paginas que não estava pegando

--- index-login.php
+++ index-login.php
@@ -3,28 +3,19 @@
 <html lang="en">
 <head>
   <title>Balada Top </title>
-  <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="index.css">
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="index.css">
   <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script src="fontawesome/web-fonts-with-css/css/fontawesome-all.min.css"></script>
-  <script src="fontawesome/svg-with-js/js/fontawesome-all.min.js"></script>
-
-  <!-- <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="index.css">
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
   <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <script src="js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="js/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   <script src="fontawesome/web-fonts-with-css/css/fontawesome-all.min.css"></script>
-  <script src="fontawesome/svg-with-js/js/fontawesome-all.min.js"></script> -->
+  <script src="fontawesome/svg-with-js/js/fontawesome-all.min.js"></script>
+
   <style>
       #map {
         height: 80%;
@@ -59,58 +50,37 @@
       }
 
   </style>
-
 </head>
-<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
-<nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#myPage">Balada Top</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
+<body>
+
+
+<nav class="navbar navbar-expand-sm navbar-light bg-faded">
+   <a class="navbar-brand" href="index.php">Balada Top</a>
+  </div>
+    <div class="collapse navbar-collapse justify-content-end" id="nav-content">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="index.php"><i class="fas fa-home"></i> HOME</a></li>
-       
-        
-        <li class="dropdown">
-          
-          
-          <ul class="dropdown-menu">
-          
-          </ul>
-        </li>
-        <!--  tag li para ir no mapa -->
-        <li><a href="mapa.php">MAPA<i class="fas fa-map-marker-alt"></i></span></a></li>
- 
-        Bem Vindo, <?php echo $_SESSION['user-logged'];?>
-<button>
+        <li class="nav-item"><a class="nav-link" href="index.php"><i class="fas fa-home fa-2x"></i> HOME </a></li>
+        <li class="nav-item"><a class="nav-link" href="mapa.php"><i class="fas fa-map-marker-alt fa-2x"></i> MAPA </a></li>
+        <li class="nav-item"><a class="nav-link" href="form_cadastro.php"><i class="fas fa-users fa-2x"></i> CADASTRE-SE </a></li>
+        <li class="nav-item"><a class="nav-link" href="login.php"> LOGIN <i class="fas fa-sign-in-alt fa-2x"></i></a></li>
+        Bem Vindo(a), <?php echo $_SESSION['user-logged'];?></li>
+		<button>
   
-<a href="login.php">Sair</a> 
-</button>
+		<a href="login.php">Sair</a> 
+		</button>
         
       </ul>
     </div>
   </div>
-</nav>
+</nav>  
+ <input id="buscar" type="text" name="buscar">
+ <input type="submit" value="Procurar">
 
-
-
-
-
-	 <input id="buscar" type="text" name="buscar">
-   <input type="submit" value="Procurar">
-
-    <div id="map"></div>
-      <!-- <div class="header"> -->
+ <div id="map"></div>
+     <!-- <div class="header"> -->
      
     <!-- </div> -->
-
+<?php include 'footer.php';?>
     <script>
       function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
