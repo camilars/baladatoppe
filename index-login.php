75a3f83830002956383028873121c8d344edd6b1
RafaelHerculano <rafaelh.silva31@gmail.com>
2018-05-02 11:49:48 -0300

    Editado css por Rafael

--- index-login.php
+++ index-login.php
@@ -98,12 +98,78 @@
 					<li style=" font-size: 14px !important;text-decoration: none; margin-top: 15px; color:gray; "><i class="fas fa-user"></i><?php echo "&nbsp;".$_SESSION['user-logged'];?> </li> 
 
 
+					<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+    font-family: "Lato", sans-serif;
+}
 
+.sidenav {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #111;
+    overflow-x: hidden;
+    transition: 0.5s;
+    padding-top: 60px;
+}
 
-					<button style=" font-size: 14px !important;text-decoration: none; margin-top: 15px;">
+.sidenav a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #818181;
+    display: block;
+    transition: 0.3s;
+}
 
-						<a href="logout.php">Sair</a> 
-					</button>
+.sidenav a:hover {
+    color: #f1f1f1;
+}
+
+.sidenav .closebtn {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+    margin-left: 50px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+</style>
+</head><link rel="stylesheet" type="text/css" href="cssteste.css">
+<body>
+<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">Editar conta</a>
+  <a href="#">Cadastrar balada</a>
+  <a href="logout.php">Sair</a>
+</div>
+
+<script>
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+</script>
+     
+</body>
+</html> 
+
 
 				</ul>
 			</div>
