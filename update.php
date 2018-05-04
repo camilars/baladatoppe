1468f1880cabdb4f36ab9f54f8a763cbe6356530
pedrolucass99 <pedrorochi73@gmail.com>
2018-05-04 08:16:34 -0300

    edit

--- update.php
+++ update.php
@@ -5,10 +5,20 @@
 	<title></title>
 </head>
 <link rel="stylesheet" type="text/css" href="update.css">
-<body>
 
+<body>
+<h1>Editar</h1>
+<form action="update.php" method="POST">
+Login :	<input type="text" name="login" value="" required><br><br>
+Senha :	<input type="password" name="senha" value="" required><br><br>
+		<input type="hidden" value="<? $linha ?>">
+	<input type="submit" value="Editar">
+</form>
 </body>
 </html>
+
+
+
 
 
 <?php include "rodape.php";?>
