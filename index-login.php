31098a254df3a33680b00535e1f8e4563b7a01d6
pedrolucass99 <pedrorochi73@gmail.com>
2018-05-04 07:23:41 -0300

    novo

--- index-login.php
+++ index-login.php
@@ -141,7 +141,7 @@
 
 					<div id="mySidenav" class="sidenav">
 						<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-						<a href="#">Editar conta</a>
+						<a href="update.php">Editar conta</a>
 						<a href="musica.php">Playlist</a>
 						<a href="logout.php">Sair</a>
 					</div>
@@ -422,16 +422,7 @@ async defer></script>
 
 </body>
 
-<h2 style= "position: absolute;top: 50px; left: 1400px; text-shadow:1px 1px 0 #444" >Music Play</h2>
-<audio controls autoplay id="audio" style="position: absolute; left:1400px; top:120px;">
-	<source " src="Jamiroquai - Superfresh Oliver Heldens Remix [thezippyaudio1.com].mp3" loop="1" width="200" height="50" type="audio/ogg" >
-		Your browser does not support the audio element.
-	</audio>
-	<!-- <embed src="Jamiroquai - Superfresh Oliver Heldens Remix [thezippyaudio1.com].mp3" autostart="true" loop="1" width="200" height="50"></embed> -->
-	<script>
-		var audio = document.getElementById('audio');
-		audio.volume = 0.5;
-	</script>
+
 
 	</html>
 	<p id="teste">
---
+++ update.php
@@ -0,0 +1,14 @@
+<?php include "header.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<link rel="stylesheet" type="text/css" href="update.css">
+<body>
+
+</body>
+</html>
+
+
+<?php include "rodape.php";?>
