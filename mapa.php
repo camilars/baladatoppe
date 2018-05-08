d09d53088f8f7351e090a6b3fe71c734fe264a81
carlosMiguelz <carlos.miguel.r@outlook.com>
2018-05-08 15:15:48 -0300

    localizador no mapa

--- mapa.php
+++ mapa.php
@@ -1,61 +1,69 @@
 <!DOCTYPE html>
 <html>
 <?php include "header.php";?>
-  <head>
-    <title>Styled Maps - Night Mode</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <style>
-      #map {
-        height: 650px;
-        width: 100%;
-        margin-top:320px ;
-       
-      }
-      .header{
-        font-size: 40px;
-        padding: 2px 2px;
-        text-align: center;
-        background-color: grey;
-        color:#000000;
 
-      }
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
-    <!DOCTYPE html>
-<html>
-  <head>
-    <style>
-       #map {
-        height: 400px;
-        width: 100%;
-       }
-    </style>
-  </head>
-  <body>
-    <h3>My Google Maps Demo</h3>
-    <div id="map"></div>
-    <script>
-      function initMap() {
-        var uluru = { lat: -7.8292242, lng: -34.9016204};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
-    </script>
-    <script async defer
+<body>
+
+<div style="margin-top:300px ;" >
+<p id="demo" style="font-size: 24px;">Clique no botão para obter sua localização:</p>
+<button onclick="getLocation()" style="margin-left:48%;">Clique aqui</button>
+<div id="mapholder"></div>
+  <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCXxe_P1-dfG5zlclrPO-fmEZNka6-KrFs&callback=initMap">
     </script>
-  </body>
+<script>
+var x=document.getElementById("demo");
+function getLocation()
+  {
+  if (navigator.geolocation)
+    {
+    navigator.geolocation.getCurrentPosition(showPosition,showError);
+    }
+  else{x.innerHTML="Geolocalização não é suportada nesse browser.";}
+  }
+ 
+function showPosition(position)
+  {
+  lat=position.coords.latitude;
+  lon=position.coords.longitude;
+  latlon=new google.maps.LatLng(lat, lon)
+  mapholder=document.getElementById('mapholder')
+  mapholder.style.height='450px';
+  mapholder.style.width='100%';
+ 
+  var myOptions={
+  center:latlon,zoom:14,
+  mapTypeId:google.maps.MapTypeId.ROADMAP,
+  mapTypeControl:false,
+  navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+  };
+  var map=new google.maps.Map(document.getElementById("mapholder"),myOptions);
+  var marker=new google.maps.Marker({position:latlon,map:map,title:"Você está Aqui!"});
+  }
+ 
+function showError(error)
+  {
+  switch(error.code)
+    {
+    case error.PERMISSION_DENIED:
+      x.innerHTML="Usuário rejeitou a solicitação de Geolocalização."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML="Localização indisponível."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML="O tempo da requisição expirou."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML="Algum erro desconhecido aconteceu."
+      break;
+    }
+  }
+</script>
+</div>
+</body>
+
+
 </html>
 
 
